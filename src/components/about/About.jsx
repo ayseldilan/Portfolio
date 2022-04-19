058ce17a33c3c21af416a6bb6e185e0f880c26b4
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-04-19 22:07:25 +0100

    adding about section and correcting images

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assets/me-about.jpg'
+import ME from '../../assets/diloo2.jpg'
 import {FaAward} from 'react-icons/fa'
 import {BiCodeAlt} from 'react-icons/bi'
 import {VscFolderLibrary} from 'react-icons/vsc'
@@ -18,7 +18,7 @@ const About = () => {
           </div>
         </div>
 
-          <div className="about__content">
+        <div className="about__content">
             <div className="about__cards">
               <article className='about__card'> 
                 <FaAward className='about__icon'/>
@@ -36,6 +36,24 @@ const About = () => {
                 <small>10+ Completed</small>
               </article>
             </div>
+
+            <p>
+            Since graduating Makers Academy’s Software Engineering accelerator program, 
+I have published a number of unique projects in an agile environment consisting 
+of daily pair-programming and completing weekly code-review. 
+
+Prior to Makers, I gained 4 years of experience in English Online Consultant and 
+gained great interpersonal skills, fostering teamwork, establishing positive 
+relations with clients, and implementing new procedures to enhance 
+operational efficiency. I’ve always looked for and developed applications to 
+improve team efficiency and solving problems.
+
+I’m ready to make an immediate impact as a Full Stack Engineer and I’m      
+confident I have the skills to develop impressive, scalable and reliable software 
+products with a positive impact in the world. 
+
+            </p>
+            <a href='#contact' className='btn btn-primary'>Let's Talk</a>
           </div>
       </div>
     </section>
